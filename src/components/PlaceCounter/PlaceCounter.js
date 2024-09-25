5b86108d01e09782f2153f7f262e0ba6d455825f
deezak <dina.m.zakaria@gmail.com>
2024-09-25 16:45:37 -0400

    formattnig home page

--- src/components/PlaceCounter/PlaceCounter.js
+++ src/components/PlaceCounter/PlaceCounter.js
@@ -18,13 +18,13 @@ const PlaceCounter = ({ countryCount, cityCount, loading  }) => {
       <div className= "flex-across-counter">
         <div className="counter-unit">
         {isDesktop && <h1 className='placecounter-label'>Countries Visited:</h1>}
-        {(isMobile || isTablet) && <h1 className='placecounter-label'>Countries Visited:</h1>} 
+        {(isMobile || isTablet) && <h1 className='placecounter-label'>Countries<br/>Visited:</h1>} 
           <p className='placecounter-number'>{countryCount}</p>
           {/* <br/> */}
         </div>
         <div className="counter-unit">
         {isDesktop && <h1 className='placecounter-label'>Cities Visited:</h1>}
-        {(isMobile || isTablet) && <h1 className='placecounter-label'>Cities Visited:</h1>} 
+        {(isMobile || isTablet) && <h1 className='placecounter-label'>Cities<br/>Visited:</h1>} 
           <p className='placecounter-number'>{cityCount}</p>
           {/* <br/> */}
         </div>
@@ -32,12 +32,12 @@ const PlaceCounter = ({ countryCount, cityCount, loading  }) => {
       <div className= "flex-across-counter">
         <div className="counter-unit">
         {isDesktop && <h1 className='placecounter-label'>Days on the Road:</h1>}
-        {(isMobile || isTablet) && <h1 className='placecounter-label'>Days on the Road:</h1>} 
+        {(isMobile || isTablet) && <h1 className='placecounter-label'>Days on<br/>the Road:</h1>} 
             <p className='placecounter-number'>453</p>
         </div> 
         <div className="counter-unit">
           {isDesktop && <h1 className='placecounter-label'>Current Country:</h1>}
-        {(isMobile || isTablet) && <h1 className='placecounter-label'>Current  Country:</h1>} 
+        {(isMobile || isTablet) && <h1 className='placecounter-label'>Current<br/>Country:</h1>} 
             <p className='placecounter-number'>USA</p>
         </div> 
       </div>     
